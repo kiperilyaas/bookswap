e2b800a900591122b581941d3b8f5feed03adce9
kiperilyaas <kiper.illia.m19@isit100.fe.it>
2026-03-30 22:38:51 +0400

    finish login implementation

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -1,12 +1,14 @@
 <?php 
 defined("APP") or die("ACCESSO NEGATO");
+require_once("models/user_model.php");
+
 
 class LoginController{
     private $model;
 
     public function __construct()
     {
-        $this->model = 0;
+        $this->model = new UserModel();
     }
 
     public function login(){
@@ -14,7 +16,39 @@ class LoginController{
     }
 
     public function check(){
-        die("ENTRATO");
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $credenziali = $this->model->findUserByMail([$email]);
+
+        if(password_verify($password, $credenziali[0]['password'])){
+            $_SESSION['id_user'] = $credenziali[0]['id_user'];
+            header("location: index.php");
+            exit;
+        }
+        else{
+            header("location: index.php?table=login&action=loginError");
+            exit;
+        }
+    }
+
+    public function register(){
+        include "views/register.php";
+    }
+
+    public function insert(){
+        $name = $_POST['name'];
+        $surname = $_POST['surname'];
+        $class = $_POST['class'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+        $param = [$name, $surname, $class, $email, $password_hash];
+        $this->model->insertRecord($param);
+
+        header("location: index.php?table=login&action=login");
+        exit;
     }
 }
 
