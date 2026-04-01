e8f48c73ae68faff15a36957da3732ad4e506e7a
KIPER ILLIA <KIPER.ILLIA.M19@isit100.fe.it>
2026-04-01 13:51:33 +0200

    start login error implementation

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -2,9 +2,30 @@
 defined("APP") or die("ACCESSO NEGATO");
 require_once("models/user_model.php");
 
-
 class LoginController{
     private $model;
+    private $emailVerified = [
+        "google.com",
+        "amazon.it",
+        "wikipedia.org",
+        "github.com",
+        "apple.com",
+        "microsoft.it",
+        "linkedin.com",
+        "netflix.com",
+        "wordpress.org",
+        "italia.it",
+        "yahoo.com",
+        "adobe.com",
+        "cloudflare.com",
+        "stackoverflow.com",
+        "reddit.com",
+        "medium.com",
+        "dropbox.com",
+        "spotify.com",
+        "zoom.us",
+        "nasa.gov"   
+    ];
 
     public function __construct()
     {
@@ -16,8 +37,17 @@ class LoginController{
     }
 
     public function check(){
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+        //email check
+        $email = $_POST['email'] ?? null;
+        if($email != null){
+            if(!in_array($email, $this->emailVerified)){
+                $_SESSION["error"][] = "dominio di email non verificato";
+                header("location: index.php?table=error&action=errorview");
+                exit;
+            }
+        }
+        //password check
+        $password = $_POST['password'] ?? null;
         $credenziali = $this->model->findUserByMail([$email]);
 
         if(password_verify($password, $credenziali[0]['password'])){
@@ -26,7 +56,8 @@ class LoginController{
             exit;
         }
         else{
-            header("location: index.php?table=login&action=loginError");
+            $_SESSION['error'][] = "La password non e' valida";
+            header("location: index.php?table=error&action=errorview");
             exit;
         }
     }
@@ -36,11 +67,30 @@ class LoginController{
     }
 
     public function insert(){
-        $name = $_POST['name'];
-        $surname = $_POST['surname'];
-        $class = $_POST['class'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+        $name = $_POST['name'] ?? null;
+        $surname = $_POST['surname'] ?? null;
+        $class = $_POST['class'] ?? null;
+        $email = $_POST['email'] ?? null;
+        $password = $_POST['password'] ?? null;
+
+        //check name and surname
+        $url = "https://lab.isit100.fe.it/api/studenti.php?elencoclassi";
+        $response = file_get_contents($url);
+        $classi = json_decode($response, true);
+
+        foreach($classi as $classe){
+            if(!in_array($class, $classe)){
+                $_SESSION['error'][] = "Classe insesitente";
+                header("location: index.php?table=error&action=error");
+                exit;
+            }
+        }
+
+        if(!in_array($email, $this->emailVerified)){
+            $_SESSION['error'][] = "dominio del email non esiste";
+            header("location: index.php?table=error&action=error");
+            exit;
+        }
 
         $password_hash = password_hash($password, PASSWORD_BCRYPT);
 
