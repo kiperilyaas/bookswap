3d1057c8e9d0e17f24801f5e54cb36de0ee1ac22
KIPER ILLIA <KIPER.ILLIA.M19@isit100.fe.it>
2026-05-09 08:42:43 +0200

    remove cloudec

--- claudecode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export ANTHROPIC_BASE_URL="http://localhost:20128/v1"
-export ANTHROPIC_AUTH_TOKEN=""
-export ANTHROPIC_API_KEY=""
-export ANTHROPIC_MODEL="kr/claude-sonnet-4.5"
-export ANTHROPIC_SMALL_FAST_MODEL="kr/claude-sonnet-4.5"
-export CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC=1
-
-claude "$@"
